f8ab92136ea2aa7d8c85e3d8d57aa0d303ec10cd
1nonly <only@localhost.localdomain>
2024-11-07 03:54:44 +0100

    owen push babe

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,7 +1,23 @@
 #include "libft.h"
+
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t dst_l;
-    size_t src_l;
-    
+    size_t src_len;
+    size_t dst_len;
+
+    src_len = ft_strlen(src);
+    dst_len = ft_strlen(dst);
+
+    if (dst_len >= dstsize)
+        return (dstsize + src_len);
+
+    if (src_len < dstsize - dst_len)
+        ft_memcpy(dst + dst_len, src, src_len + 1);
+    else
+    {
+        ft_memcpy(dst + dst_len, src, dstsize - dst_len - 1);
+        dst[dstsize - 1] = '\0';
+    }
+
+    return (dst_len + src_len);
 }
